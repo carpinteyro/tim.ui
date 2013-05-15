f0935e16a521daba53664273862fa8a4ca3bfd82
Omar Carpinteyro <dracoryet@gmail.com>
2013-05-15 09:53:23 -0500

    v1.0.0
    Primera versión

---
+++ js/tim.ui.core-1.0.0.js
@@ -0,0 +1,14 @@
+﻿/*! Televisa Interactive Media UI JS - v1.0.0 - 15/05/2013
+* http://www.televisa.com
+* Includes:
+* tim.ui.core.js
+*/
+
+$(function(){
+	
+});
+
+function autorun(){}
+if (window.addEventListener) window.addEventListener("load", autorun, false);
+else if (window.attachEvent) window.attachEvent("onload", autorun);
+else window.onload = autorun;
